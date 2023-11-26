fd63c1100da5aaa5bb5446d52ef86386e02ea991
Harsh <61960651+harshsindhu0408@users.noreply.github.com>
2023-11-26 21:35:33 +0530

    created_endpoints

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  const Links = [
+    { id: 1, name: "Profile", link: "/profile" },
+  ];
+
   return (
-    <div>
-      
-    </div>
+    <nav className="w-full z-10 relative bg-gray-800 h-20 flex items-center justify-center shadow-2xl">
+      <div className="w-11/12  flex flex-row items-center justify-between">
+
+      </div>
+    </nav>
   )
 }
 
