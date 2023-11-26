68d485dd1f55c89987a921faa3ca965c7fbc90fe
Harsh <61960651+harshsindhu0408@users.noreply.github.com>
2023-11-26 22:18:34 +0530

    createdNavbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-
   const Links = [
-    { id: 1, name: "Profile", link: "/profile" },
+    {id: 1, name: "Profile", link: "/profile" },
+    {id: 1, name: "Create NOC", link: "/create-noc" },
+    {id: 1, name: "Download NOC", link: "/download-noc" },
   ];
-
+  const navigate = useNavigate();
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const loginHandler = () => {
+    navigate("/login");
+  };
   return (
     <nav className="w-full z-10 relative bg-gray-800 h-20 flex items-center justify-center shadow-2xl">
       <div className="w-11/12  flex flex-row items-center justify-between">
+        {/* Left side */}
+        <Link to={'/'}>
+        <div className="text-lg font-bold font-Roborto text-white">StudentCorner</div>
+        </Link>
 
+        {/* Mid div with links */}
+        <div className="hidden md:block">
+          <ul className="flex items-center justify-center gap-10">
+            {Links.map((link) => (
+              <li key={link.id}>
+                <Link
+                  to={link.link}
+                  className="text-white font-bold font-Roborto hover:text-indigo-500 duration-200"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Login and logout button */}
+        <div>
+          {isLoggedin ? (
+            <button
+              className="bg-indigo-500 hover:bg-indigo-600 hover:scale-95 block font-bold text-white shadow-sm rounded-full px-4 py-2 duration-300 w-24"
+              onClick={() => navigate("/login")}
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              className="bg-indigo-500 hover:bg-indigo-600 hover:scale-95 block font-bold text-white shadow-sm rounded-full px-4 py-2 duration-300 w-24"
+              onClick={loginHandler}
+            >
+              Login
+            </button>
+          )}
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
