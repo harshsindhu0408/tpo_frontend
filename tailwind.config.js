2dbede039b9e4394c014857dcbb497360cf30eb5
Harsh <61960651+harshsindhu0408@users.noreply.github.com>
2023-11-26 22:29:25 +0530

    updated_navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "250px",
+      md: "1000px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      fontFamily: {
+        Roborto: ["sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
